0e218f5f24f387e839713b9acae9253a826b3a16
iamwesofph <piuswesley@gmail.com>
2023-09-08 23:32:04 +0800

    Finish MVP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+    res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+    const username = req.body.username;
+    const message = req.body.message;
+    messages.push({ text: message, user: username, added: new Date() });
+    res.redirect("/");
 });
 
 module.exports = router;
